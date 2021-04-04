b0e26466ebdb7141412a89681b2a21903c4ae84e
Gian Zamora <gz2016@att.net>
2021-04-04 13:12:14 -0700

    first commit with README creation functionality

---
+++ index.js
@@ -0,0 +1,84 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a brief introduction/description of the project:'
+    },
+    {
+        type: 'input',
+        name: 'accomplished',
+        message: 'Give a description of how you accomplished the project:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Describe how to install(if applicable):'
+    },
+    {
+        type: 'input',
+        name: 'uses',
+        message: 'Describe how the project can be used/is meant to be used: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license to guard this project',
+        choices: [
+            'MIT',
+            'Apache', 
+            'GPL',
+            'ISC',
+            'Mozilla', 
+            'Open'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who else contributed to this project(add commas in between each username)?'
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Please enter the Github Repo link:'
+    },
+    {
+        type: 'input',
+        name: 'livelink',
+        message: 'Please enter the live webiste link:'
+    },
+]
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+// function init() {}
+const init = () => {
+   inquirer.prompt(questions)
+   .then((answers) => {
+       const {title, description, accomplished, install, uses, license, username, contributors, repo, livelink} =  answers;
+       const string = `# ${answers.title}\n\n## Introduction\n ${answers.description}\n\n## How the project was accomplished\n ${answers.accomplished}\n\n## Installation Guide\n ${answers.install}\n\n## Usability\n ${answers.uses}\n\n## Links to Gihub Repository and Live Site\n Github Repo: ${answers.repo}\n Live Site: ${answers.livelink}\n\n## Creator and Contributors\n Creator: ${answers.username}\n Contributors: ${answers.contributors}`;
+       fs.writeFile('README.md', string, (err) => {
+           if (err) throw err;
+           console.log('The file has been saved.');
+        }); 
+   }) 
+}
+
+// Function call to initialize app
+init();
